47b25763b610680e9f5ed4af57d6650c7a716901
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-13 11:40:34 -0400

    add link to index, add divs to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from '../styles/logo.svg';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="title">
+          <div className="my-header">
+          "Earthquakes!"
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
